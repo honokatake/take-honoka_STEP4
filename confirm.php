0c399e802ad8e8241b296f714e40ef54a2d21e67
honokatake <honoka.mac.no@gmail.com>
2026-04-06 15:24:56 +0900

    フォーム完成

---
+++ confirm.php
@@ -0,0 +1,57 @@
+<?php
+$name = $_POST['name'];
+if (!preg_match("/^[ぁ-んァ-ン一-龥]+$/u", $name)){
+        echo "名前はひらがな、カタカナ、漢字のみ使用できます。";
+        exit;
+}
+$age = $_POST['age'];
+if (!is_numeric($age) || $age < 0 || $age > 150){
+        echo "年齢は0から150の間で入力してください。";
+        exit;
+}
+
+$phone = $_POST['phone'];
+if(!preg_match("/^[0-9\-]+$/", $phone)){
+    echo "電話番号は半角数字とハイフンのみ使用できます。";
+    exit;
+}
+
+$email = $_POST['email'];
+if(!filter_var($email,FILTER_VALDATE_EMAIL)){
+    echo "メールアドレスの形式が正しくありません。";
+    exit;
+}
+
+$address = $_POST['address'];
+if(!preg_match("/^[ぁ-んァ-ン一-龥a-zA-Z0-9\s\-]+$",$address)){
+    echo "住所はひらがな、カタカナ、漢字、英字のみ使用できます。";
+    exit;
+}
+
+
+$agreement = $_POST['agreement'];
+
+
+$gender = $_POST['gender'];
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+<meta charset="UTF-8">
+<title>確認画面</title>
+</head>
+
+<body>
+
+<h1>入力内容確認</h1>
+
+<p>お名前：<?php each $name;?></p>
+<p>年齢：<?php each $age;?></p>
+<p>住所：<?php each $address?></p>
+<p>質問：<?php each $agreement?></p>
+<p>性別：<?php each $gender?></p>
+
+
+</body>
+</html>
---
+++ form.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ステップ４　フォーム</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<main>
+<section class="form-aria">
+<h1>フォーム入力</h1>
+
+<div class="form-container">
+<form action="confirm.php" method="post">
+<p>お名前：<br><input type="text" name="name" pattern="^[ぁ-んァ-ン一-龥]+$"></p>
+<p>年齢：<br><input type="text" name="age" min="0" max="150" required></p>
+<p>電話番号：<br><input type="text" name="phone"></p>
+<p>メールアドレス：<br><input type="text" name="email" pattern="^[ぁ-んァ-ン一-龥a-zA-Z0-9\s\-]+$"></p>
+<p>住所：<br><input type="text" name="address"></p>
+<p>質問：<br><input type="text" name="agreement"></p>
+<p>性別：<br><select name="gender" id="gender">
+<option>男性</option>
+<option>女性</option>
+<option>その他</option></select></p>
+<button type="submit">送信</button>
+</form>
+</div>
+
+</section>
+</main>
+
+
+</body>
+</html>
